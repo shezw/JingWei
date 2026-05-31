1a41f5034f27f797c04c9faecfefc1aa9dae5513
shezw <hello@shezw.com>
2026-05-31 12:24:05 +0800

    Add backend build switches

---
+++ src/platform/backend/fbdev/jw_backend_fbdev_stub.c
@@ -0,0 +1,10 @@
+#include "jw_internal.h"
+
+jw_proxy_t *jw_proxy_create_fbdev(const char *device, int width, int height, const char *title)
+{
+    (void)device;
+    (void)width;
+    (void)height;
+    (void)title;
+    return NULL;
+}
---
+++ src/platform/backend/sdl/jw_backend_sdl_stub.c
@@ -0,0 +1,9 @@
+#include "jw_internal.h"
+
+jw_proxy_t *jw_proxy_create_sdl(int width, int height, const char *title)
+{
+    (void)width;
+    (void)height;
+    (void)title;
+    return NULL;
+}
